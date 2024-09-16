73dca7a026c7efd7d3fc7c7c890344c2f9645dcd
Geetanshgarg <90083776+Geetanshgarg@users.noreply.github.com>
2024-09-16 07:10:48 +0000

    Pending changes exported from your codespace

--- components/nav.js
+++ components/nav.js
@@ -1,28 +1,28 @@
 "use client";
 import Link from "next/link";
+import "./style.css"
 export default function Nav() {
     return ( 
         <div className="w-full fixed h-14 text-white items-center px-6 pt-3 ">
         <nav className="flex mx-auto items-center justify-between">
-          <div className="text-2xl font-cuba">OnlinePlayer</div>
+          <div className=" logo text-2xl font-recoleta">OnlinePlayer</div>
           <ul className="list-none flex gap-5 ">
-            <li><Link href="#" className="m-2 hover:text-slate-100 ">
+            <li><Link href="#" className="m-2 hover:text-slate-200 ">
             Features
           </Link></li>
-          <li><Link href="#" className="m-2 hover:text-slate-100 ">
+          <li><Link href="#" className="m-2 hover:text-slate-200 ">
             Product
           </Link></li>
-          <li><Link href="#" className="m-2 hover:text-slate-100">
+          <li><Link href="#" className="m-2 hover:text-slate-200">
             About Us
           </Link></li>
           </ul>
-          <div className="px-2"><Link href="#" className="m-2 hover:text-slate-100">
+          <div className="px-2"><Link href="#" className="m-2 hover:text-slate-200">
             Login
-          </Link><Link href="#" className="m-2 text-black rounded-3xl hover:bg-white bg-slate-50 p-3 ">
+          </Link><Link href="#" className="m-2 text-black rounded-3xl bg-white hover:bg-slate-200 p-3 ">
             Sign Up
           </Link></div>
         </nav>
-        {/* <span className="w-full border-t border-gray-300"></span> */}
       </div>
       
     )
--- components/trial.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const trial = () => {
-  return (
-    <div className='h-64 bg-gradient-to-t from-teal-400 via-emerald-500 to-green-600'></div>
-  )
-}
-
-export default trial
